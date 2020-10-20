7934242dd5939d6db124abc9bcbd3ce60b149236
Ji_Rath <garethaguilar@gmail.com>
2020-10-20 18:37:06 -0500

    Refactor EventSystem again - events will now be properly removed when finished - add comments to event code - add explosion event - move UI animation to their respective folder

--- ScreamJam2020/Assets/Scripts/Events/EventTimedBase.cs
+++ ScreamJam2020/Assets/Scripts/Events/EventTimedBase.cs
@@ -7,10 +7,46 @@ public abstract class EventTimedBase : EventBase
     [Range(0f,60f)]
     public float eventTime = 0;
 
-    public override void OnEventTrigger()
+    private Coroutine timedEvent;
+
+    //Called when an event wants to be triggered
+    public override bool OnEventTrigger()
     {
-        StartCoroutine(TimedEvent());
+        if (CanTrigger)
+        {
+            if (!MultipleTriggers)
+                HasTriggered = true;
+            else
+                StartCoroutine(EventCoolDown());
+
+            EventTrigger();
+            CanTrigger = false;
+
+            return true;
+        }
+
+        return false;
     }
 
+    //Called if the event can be triggered
+    public override void EventTrigger()
+    {
+        //Trigger timed event
+        timedEvent = StartCoroutine(TimedEvent());
+
+        //wait for timed event to finish to trigger
+        StartCoroutine(WaitEventCompletion());
+    }
+
+    //Called to perform actual timed event
     public abstract IEnumerator TimedEvent();
+
+    //Called when TimedEvent is finished
+    public IEnumerator WaitEventCompletion()
+    {
+        yield return timedEvent;
+
+        if (HasTriggered)
+            Destroy(gameObject);
+    }
 }
